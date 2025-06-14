06fd66fc2a86979e16640c183db9210a0e31f14a
Pedro Henrique Freitas <1pedrohfreitas@gmail.com>
2025-06-14 12:16:45 -0300

    Initial commit

---
+++ src/main/java/dev/pedrohfreitas/accessgate/AccessgateApplication.java
@@ -0,0 +1,13 @@
+package dev.pedrohfreitas.accessgate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessgateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessgateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pedrohfreitas/accessgate/AccessgateApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pedrohfreitas.accessgate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessgateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
